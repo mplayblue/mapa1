24811977c583f5e6139a3c98689b4bbcc420023f
Mi <40923306+mplayblue@users.noreply.github.com>
2025-12-04 17:55:58 -0300

    Create .js

---
+++ .js
@@ -0,0 +1,38 @@
+// mapa-filmes-1.js
+const MAPA_FILMES_1 = {
+    "vingadores-ultimato": {
+        titulo: "Vingadores: Ultimato",
+        capa: "https://upload.wikimedia.org/wikipedia/pt/c/c0/Bride-of-Chucky.jpg",
+        urlPost: "/2025/11/vingadores-ultimato.html" 
+    },
+    "homem-aranha-longe": {
+        titulo: "Homem-Aranha: Longe de Casa",
+        capa: "https://upload.wikimedia.org/wikipedia/pt/c/c0/Bride-of-Chucky.jpg",
+        urlPost: "/2025/11/homem-aranha-longe.html"
+    },
+    "a-noiva-chucky-1": {
+        titulo: "A Noiva de Chucky (Capa 1)",
+        capa: "https://upload.wikimedia.org/wikipedia/pt/c/c0/Bride-of-Chucky.jpg",
+        urlPost: "https://m3uplay007.blogspot.com/2025/11/blog-post_17.html"
+    },
+    "a-noiva-chucky-2": {
+        titulo: "A Noiva de Chucky (Capa 2)",
+        capa: "https://upload.wikimedia.org/wikipedia/pt/c/c0/Bride-of-Chucky.jpg",
+        urlPost: "https://m3uplay007.blogspot.com/2025/11/blog-post_17.html"
+    },
+    "a-noiva-chucky-3": {
+        titulo: "A Noiva de Chucky (Capa 3)",
+        capa: "https://upload.wikimedia.org/wikipedia/pt/c/c0/Bride-of-Chucky.jpg",
+        urlPost: "https://m3uplay007.blogspot.com/2025/11/blog-post_17.html"
+    },
+    "morte-de-um-unicornio": {
+        titulo: "Morte de um Unicórnio",
+        capa: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcR05hVxr-0wf-hWmKYlD-w1KwkzmxSUp1h8zVVzmXriU84An4vQ",
+        urlPost: "https://m3upl.blogspot.com/2025/11/filmes-lancamentosmorte-de-um-unicornio.html"
+    },
+    "panico-na-ilha": {
+        titulo: "Pânico na Ilha",
+        capa: "https://m.media-amazon.com/images/M/MV5BMTQyMjc1NjQ4Nl5BMl5BanBnXkFtZTcwOTQ4ODM4Mg@@._V1_FMjpg_UX1000_.jpg",
+        urlPost: "https://m3upl.blogspot.com/2025/11/panico-na-ilha.html"
+    }
+};
